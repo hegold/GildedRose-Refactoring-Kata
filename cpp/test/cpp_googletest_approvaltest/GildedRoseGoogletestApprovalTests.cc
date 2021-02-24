bf07451f631900f230bc187cc702a90d805e1990
Henry Golding <hegold@users.noreply.github.com>
2021-02-23 20:45:46 -0800

    Approvaltests

--- cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
+++ cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
@@ -15,9 +15,17 @@ std::ostream& operator<<(std::ostream& os, const Item& obj)
 
 TEST(GildedRoseApprovalTests, VerifyCombinations) {
 
-    std::vector<string> names { "Foo" };
-    std::vector<int> sellIns { 1 };
-    std::vector<int> qualities { 1 };
+	std::vector<string> names { 
+		"+5 Dexterity Vest",
+		"Foo",
+		"Aged Brie",
+		"Elixir of the Mongoose",
+		"Sulfuras, Hand of Ragnaros",
+		"Backstage passes to a TAFKAL80ETC concert",
+		"Conjured Mana Cake" 
+	};
+    std::vector<int> sellIns { -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+    std::vector<int> qualities { -1, 0, 6, 7, 19, 20, 29, 30, 49, 80, 1000 };
 
     auto f = [](string name, int sellIn, int quality) {
         vector<Item> items = {Item(name, sellIn, quality)};
